c8879824046bc6b7de20a6a53eef274c5125e066
Kirk Wight <kwight@kwight.ca>
2020-02-18 13:08:15 -0800

    move assets out of web, oops

--- lib/views/event_loop2.dart
+++ lib/views/event_loop2.dart
@@ -11,7 +11,7 @@ class EventLoop2 extends StatelessWidget {
       body: <Widget>[
         Container(
           child: Image.asset(
-            'web/images/brogdon_event_loop.jpg',
+            'images/brogdon_event_loop.jpg',
             fit: BoxFit.contain,
             height: 400,
           ),
